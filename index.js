a1db6e33067c91328c7106eda20bb939a70a10fd
Jose Ramon <jrpaz@boardfy.com>
2025-01-13 09:53:35 +0100

    wip

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const modal = document.querySelector(".modal");
 const images = document.querySelectorAll(".image-click-modal");
 const modalImage = document.querySelector(".modal-image");
 const sercvice = document.querySelector(".what-do-we-do");
-const local = document.querySelector(".place");
 const gallery = document.querySelector(".gallery");
 const joinUs = document.querySelector(".join-us");
 const contact = document.querySelector(".contact");
 const linkService = document.querySelector(".link-what-do-we-do");
-const linkLocal = document.querySelector(".link-place");
 const linkGallery = document.querySelector(".link-gallery");
 const linkJoinUs = document.querySelector(".link-join-us");
 const linkContact = document.querySelector(".link-contact");
@@ -22,19 +20,11 @@ window.addEventListener("scroll", () => {
   }
   if (
     window.scrollY >= sercvice.offsetTop - 300 &&
-    window.scrollY < local.offsetTop - 300
+    window.scrollY < gallery.offsetTop - 300
   ) {
     linkService.classList.add("active");
   } else {
     linkService.classList.remove("active");
-  }
-  if (
-    window.scrollY >= local.offsetTop - 300 &&
-    window.scrollY < gallery.offsetTop - 300
-  ) {
-    linkLocal.classList.add("active");
-  } else {
-    linkLocal.classList.remove("active");
   }
   if (
     window.scrollY >= gallery.offsetTop - 300 &&
@@ -71,30 +61,4 @@ modal.addEventListener("click", () => {
   body.style.overflow = "auto";
   modal.classList.remove("modal-open");
   modalImage.src = "";
-});
-
-new Swiper(".card-wrapper", {
-  loop: true,
-  speed: 500,
-  spaceBetween: 10,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-  },
 });
